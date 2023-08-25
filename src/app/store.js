1936af1081f19c4d181c28c9365f8ec246c6fa5c
riskarsh <utkarshukla1@gmail.com>
2023-08-25 10:05:19 +0530

    Update store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import authSlice from "./features/auth/authSlice";
 import { applyMiddleware } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import companySlice from "./features/company/companySlice";
+import reviewSlice from "./features/review/reviewSlice";
 
 // Configure the Redux store
 const store = configureStore(
@@ -10,7 +11,8 @@ const store = configureStore(
     // Define the initial state and reducers for the store
     reducer: {
       user: authSlice, // Using the 'authSlice' reducer for managing user authentication state
-      company:companySlice // Using the 'companySlice' reducer for managing company authentication state
+      company:companySlice, // Using the 'companySlice' reducer for managing company authentication state
+      review:reviewSlice // Using the 'reviewSlice' reducer for managing company review state
     },
   },
   applyMiddleware(thunk) // Apply the 'redux-thunk' middleware for handling asynchronous actions
